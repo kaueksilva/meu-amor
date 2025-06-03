c2ee7a6a35c9ae1aab4942c8f06dd757f79988e4
Kauê Silva <kaueksilva12@gmail.com>
2025-06-03 09:55:40 -0300

    ajuste para o next

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig = {
-  output: 'export', // Isso habilita a exportação estática
-  distDir: 'out', // Pasta onde os arquivos estáticos serão gerados
-  images: {
-    unoptimized: true, // Necessário para exportação estática
-  },
-  // Remova a configuração basePath se não estiver usando subdiretório
-  trailingSlash: true, // Recomendado para exportação estática
-};
-
-module.exports = nextConfig;
-
-export default nextConfig;
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+  /* config options here */
+}
+ 
+export default nextConfig
