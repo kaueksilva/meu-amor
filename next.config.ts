e1c12674a7bb53a421793c5cfd4ff51e0161c2b9
Kauê Silva <kaueksilva12@gmail.com>
2025-06-03 10:10:48 -0300

    ajuste no next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from 'next'
- 
-const nextConfig: NextConfig = {
-  /* config options here */
-}
- 
-export default nextConfig
+import type { NextConfig } from "next";
+
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
+
+export default nextConfig;
