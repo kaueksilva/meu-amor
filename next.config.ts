b77d4f94f789973231b8c91cb21d7771f39ecd55
Kauê Silva <kaueksilva12@gmail.com>
2025-06-03 10:11:34 -0300

    next

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
-import type { NextConfig } from "next";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
-
-export default nextConfig;
